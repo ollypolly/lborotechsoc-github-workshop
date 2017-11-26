49434679aadc2e452307a18ae72cf1ff95992d01
wjbaker101 <willbaker10198@gmail.com>
2017-11-26 11:35:43 +0000

    Add scrolling header function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // Detecting when the user has scrolled down
+const scrollingHeader = (() => {
+    const scrollHeader = (header, scrollY) => {
+        header.classList.toggle('scrolling', scrollY > 5);
+    };
+
+    const onWindowScroll = () => {
+        const header = document.querySelector('body > header');
+        const scrollY = window.scrollY;
+
+        scrollHeader(header, scrollY);
+    };
+
+    const initialise = () => {
+        window.addEventListener('scroll', onWindowScroll);
+    };
+
+    return {
+        init: initialise,
+    }
+})();
+
+window.addEventListener('load', scrollingHeader.init);
